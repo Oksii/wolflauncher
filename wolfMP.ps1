925387a4eabcde3c7bc5874be515003e1f4bc734
Oksii <oxymoron.et@gmail.com>
2023-12-05 16:58:50 +0100

    Update wolfMP.ps1

--- wolfMP.ps1
+++ wolfMP.ps1
@@ -1,84 +1,85 @@
 # Define variables
-$webAddress = "https://maps.oksii.eu/rtcwpro/"
+$repoOwner = "rtcwmp-com"
+$repoName = "rtcwPro"
+$releaseApiUrl = "https://api.github.com/repos/$repoOwner/$repoName/releases/latest"
+$executableNamePrefix = "wolfMP"
 $tempFolder = "temp"
-$executableName = "wolfMP.exe"
-$subdirectoryName = "rtcwpro"
 
-# Use current working directory as local folder
+# Use the current working directory as the local folder
 $localFolder = Get-Location
 
-# Construct full paths
-$tempPath = Join-Path -Path $localFolder -ChildPath $tempFolder
-$executablePath = Join-Path -Path $localFolder -ChildPath $executableName
-$subdirectoryPath = Join-Path -Path $localFolder -ChildPath $subdirectoryName
+# Fetch the release information
+$releaseInfo = Invoke-RestMethod -Uri $releaseApiUrl
 
-# Download the web index
-$webIndex = Invoke-WebRequest -Uri $webAddress
+# Extract the version from the asset name
+$asset = $releaseInfo.assets | Where-Object { $_.name -match 'rtcwpro_(\d+)_client.zip' }
 
-# Extract version from the file name on the web index
-$versionMatches = [regex]::Matches($webIndex.Content, 'rtcwpro_(\d+)_client.zip')
-if ($versionMatches.Count -eq 0) {
-    Write-Host "Error: No matching file found on the web index."
-    exit
-}
+if ($asset) {
+    $assetVersion = $matches[1]
 
-$webVersion = $versionMatches[0].Groups[1].Value
+    # Construct the local executable path
+    $localExecutablePath = Join-Path -Path $localFolder -ChildPath "$executableNamePrefix_$assetVersion.exe"
 
-# Check if local version matches
-$localVersions = Get-ChildItem -Path $localFolder -Filter "wolfMP_*.exe" | ForEach-Object {
-    [regex]::Match($_.Name, 'wolfMP_(\d+).exe').Groups[1].Value
-}
-
-if ($localVersions -contains $webVersion) {
-    Write-Host "Local version is up to date. Launching wolfMP_$webVersion.exe"
-    Start-Process "$localFolder\wolfMP_$webVersion.exe" -ArgumentList "+set fs_game rtcwpro"
-    exit
-}
-
-# Create the temp folder
-New-Item -ItemType Directory -Path $tempPath -Force | Out-Null
-
-# Delete rtcwpro_*.pk3 files if updating wolfMP_<version>.exe
-if (Test-Path $executablePath -PathType Leaf) {
-    Write-Host "Deleting rtcwpro_*.pk3 files from $subdirectoryPath..."
-    Get-ChildItem -Path $subdirectoryPath -Filter "rtcwpro_*.pk3" | Remove-Item -Force
-}
-
-# Download and extract the updated executable
-$executableUrl = "$webAddress/rtcwpro_$($webVersion)_client.zip"
-$archiveTempPath = Join-Path -Path $tempPath -ChildPath "rtcwpro_$($webVersion)_client.zip"
-
-Write-Host "Downloading $executableUrl..."
-Invoke-WebRequest -Uri $executableUrl -OutFile $archiveTempPath
-
-# Extract the contents of the downloaded archive
-Expand-Archive -Path $archiveTempPath -DestinationPath $tempPath -Force
-
-# Rename "wolfMP.exe" to "wolfMP_<version>.exe"
-$downloadedExecutablePath = Join-Path -Path $tempPath -ChildPath "wolfMP.exe"
-if (Test-Path $downloadedExecutablePath -PathType Leaf) {
-    $newExecutableName = "wolfMP_$webVersion.exe"
-    Rename-Item -Path $downloadedExecutablePath -NewName $newExecutableName -Force
-}
-
-# Move files to the root folder
-Get-ChildItem -Path $tempPath | Move-Item -Destination $localFolder -Force
-
-# Remove temp folder
-Remove-Item -Path $tempPath -Recurse -Force
-
-# Clean up: Delete the downloaded .zip archive
-Remove-Item -Path "$localFolder\rtcwpro_$($webVersion)_client.zip" -Force
-
-# Clean up: Delete any .exe files that do not match "wolfMP_<version>.exe" (excluding <version> MINUS 1)
-Get-ChildItem -Path $localFolder -Filter "wolfMP_*.exe" | ForEach-Object {
-    $currentVersion = [regex]::Match($_.Name, 'wolfMP_(\d+).exe').Groups[1].Value
-    if ($currentVersion -ne $webVersion -and $currentVersion -ne ($webVersion - 1)) {
-        Remove-Item -Path $_.FullName -Force
+    # Check if local version matches
+    $localVersions = Get-ChildItem -Path $localFolder -Filter "wolfMP_*.exe" | ForEach-Object {
+        [regex]::Match($_.Name, 'wolfMP_(\d+).exe').Groups[1].Value
     }
-}
 
-# Launch the newly downloaded executable
-$launchedExecutablePath = Join-Path -Path $localFolder -ChildPath $newExecutableName
-Write-Host "Launching $launchedExecutablePath"
-Start-Process $launchedExecutablePath -ArgumentList "+set fs_game rtcwpro"
+    if ($localVersions -contains $assetVersion) {
+        Write-Host "Local version is up to date. Launching wolfMP_$assetVersion.exe"
+        Start-Process "$localFolder\wolfMP_$assetVersion.exe" -ArgumentList "+set fs_game rtcwpro"
+        exit
+    }
+
+
+    # Create the temp folder
+    $tempPath = Join-Path -Path $localFolder -ChildPath $tempFolder
+    New-Item -ItemType Directory -Path $tempPath -Force | Out-Null
+
+    # Download the asset
+    $downloadUrl = $asset.browser_download_url
+    Write-Host "Downloading $asset.name..."
+    Invoke-WebRequest -Uri $downloadUrl -OutFile (Join-Path -Path $tempPath -ChildPath $asset.name)
+
+    # Extract the contents of the downloaded archive
+    Expand-Archive -Path (Join-Path -Path $tempPath -ChildPath $asset.name) -DestinationPath $tempPath -Force
+
+    # Delete rtcwpro_*.pk3 files if updating wolfMP_<version>.exe
+    $rtcwproPath = Join-Path -Path $localFolder -ChildPath "rtcwpro"
+    if (Test-Path $rtcwproPath -PathType Container) {
+        Write-Host "Deleting rtcwpro_*.pk3 files from $rtcwproPath..."
+        Get-ChildItem -Path $rtcwproPath -Filter "rtcwpro_*.pk3" | Remove-Item -Force
+    }
+
+    # Rename "wolfMP.exe" to "wolfMP_<version>.exe"
+    $downloadedExecutablePath = Join-Path -Path $tempPath -ChildPath "wolfMP.exe"
+    if (Test-Path $downloadedExecutablePath -PathType Leaf) {
+        $newExecutableName = "wolfMP_$assetVersion.exe"
+        Rename-Item -Path $downloadedExecutablePath -NewName $newExecutableName -Force
+    }
+
+    # Move files to the root folder
+    Get-ChildItem -Path $tempPath | Move-Item -Destination $localFolder -Force
+
+    # Clean up: Delete rtcwpro_<version>_client.zip
+    Remove-Item -Path (Join-Path -Path $localFolder -ChildPath $asset.name) -Force
+
+    # Clean up: Delete temp folder
+    Remove-Item -Path $tempPath -Recurse -Force
+
+    # Clean up: Delete any .exe files that do not match "wolfMP_<version>.exe" (excluding <version> MINUS 1)
+    Get-ChildItem -Path $localFolder -Filter "wolfMP_*.exe" | ForEach-Object {
+        $currentVersion = [regex]::Match($_.Name, 'wolfMP_(\d+).exe').Groups[1].Value
+        if ($currentVersion -ne $assetVersion -and $currentVersion -ne ($assetVersion - 1)) {
+            Remove-Item -Path $_.FullName -Force
+        }
+    }
+
+    # Launch the newly downloaded executable
+    $launchedExecutablePath = Join-Path -Path $localFolder -ChildPath $newExecutableName
+    Write-Host "Launching $launchedExecutablePath"
+    Start-Process $launchedExecutablePath -ArgumentList "+set fs_game rtcwpro"
+}
+else {
+    Write-Host "Error: No matching asset found."
+}
